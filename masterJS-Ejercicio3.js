7cb837280b3e37bca7481533abc4cb36067e01ca
LilenAmado <65418208+LilenAmado@users.noreply.github.com>
2020-08-20 19:58:57 -0300

    Add files via upload

---
+++ masterJS-Ejercicio3.js
@@ -0,0 +1,20 @@
+'use strict'
+// Hacer un programa que muestre todos los números entre dos números introducidos por el usuario.
+
+let num1 = Number(prompt("Introduzca el primer número"));
+let num2 = Number(prompt("Introduzca el segundo número"));
+
+while (isNaN(num1) || isNaN(num2)){
+    num1 = Number(prompt("Introduzca el primer número"));
+    num2 = Number(prompt("Introduzca el segundo número"));
+}
+
+document.write("Entre el: " + num1 + " y el: " + num2 + " se encuentran los números: <br><br>");
+
+for (let i = num1; i <= num2 ; i++){
+    document.write(i + "<br>");
+}
+
+for (let i = num1; i >= num2 ; i--){
+    document.write(i + "<br>");
+}
